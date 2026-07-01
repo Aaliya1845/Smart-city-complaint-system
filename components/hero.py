1ae30bc31ea7a42a71c698c8f1860cd3b1b54d07
Aaliya1845 <tasbiyaansari2207@gmail.com>
2026-07-01 22:29:59 +0530

    Update hero.py

--- components/hero.py
+++ components/hero.py
@@ -2,26 +2,23 @@ import streamlit as st
 
 def hero_section():
 
-    st.markdown("""
+    name = st.session_state.get("user_name", "Citizen")
+
+    st.markdown(f"""
     <div style="
-    background:linear-gradient(135deg,#FFDDEB,#DFF4FF);
-    padding:35px;
-    border-radius:25px;
-    box-shadow:0px 10px 25px rgba(0,0,0,.08);
-    margin-bottom:20px;
+        background:linear-gradient(135deg,#FFD9EA,#DDF4FF);
+        padding:35px;
+        border-radius:25px;
+        box-shadow:0 10px 25px rgba(0,0,0,.08);
     ">
 
-    <h1 style="color:#E75480;">
-    🏙 Smart City Complaint System
-    </h1>
+        <h1>🏙 Smart City Complaint System</h1>
 
-    <h3 style="color:#4F9DD9;">
-    Welcome 👋
-    </h3>
+        <h3>Welcome, {name} 👋</h3>
 
-    <p style="font-size:18px;">
-    AI Powered Complaint Management Platform
-    </p>
+        <p style="font-size:18px;">
+            AI Powered Citizen Service Portal
+        </p>
 
     </div>
     """, unsafe_allow_html=True)
